70e7483b7393cdba6e594c25f30a3e171b0efe2c
Seggy Umboh <seg@seg.gy>
2013-03-27 00:29:16 -0700

    Added twitter widgets js

--- public/javascripts/iou.coffee
+++ public/javascripts/iou.coffee
@@ -1,7 +1,15 @@
+$.cachedScript = (url, options) ->
+  options = $.extend options || {},
+    dataType: 'script'
+    cache: true
+    url: url
+  $.ajax options
+
 $(document).on 'mobileinit', ->
   $.mobile.defaultPageTransition = 'slide'
 .on 'pageinit', '.ledger', (e) ->
   $('time', e.target).timeago()
+  twttr?.widgets?.load()
 .on 'pageinit', '#owe-someone', (e) ->
   $('form', e.target).on 'submit', (e) ->
     e.preventDefault()
@@ -10,3 +18,6 @@ $(document).on 'mobileinit', ->
     amount = $('[name=amount]', e.target).val()
     if owee && amount
       window.location = "https://twitter.com/intent/tweet?text=#{escape("#{owee} #iou #{amount}")}"
+
+$ ->
+  $.cachedScript '//platform.twitter.com/widgets.js'
--- public/javascripts/iou.js
+++ public/javascripts/iou.js
@@ -1,9 +1,20 @@
 (function() {
 
+  $.cachedScript = function(url, options) {
+    options = $.extend(options || {}, {
+      dataType: 'script',
+      cache: true,
+      url: url
+    });
+    return $.ajax(options);
+  };
+
   $(document).on('mobileinit', function() {
     return $.mobile.defaultPageTransition = 'slide';
   }).on('pageinit', '.ledger', function(e) {
-    return $('time', e.target).timeago();
+    var _ref;
+    $('time', e.target).timeago();
+    return typeof twttr !== "undefined" && twttr !== null ? (_ref = twttr.widgets) != null ? _ref.load() : void 0 : void 0;
   }).on('pageinit', '#owe-someone', function(e) {
     return $('form', e.target).on('submit', function(e) {
       var amount, owee;
@@ -15,6 +26,10 @@
         return window.location = "https://twitter.com/intent/tweet?text=" + (escape("" + owee + " #iou " + amount));
       }
     });
+  });
+
+  $(function() {
+    return $.cachedScript('//platform.twitter.com/widgets.js');
   });
 
 }).call(this);
