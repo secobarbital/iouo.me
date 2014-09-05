1ef9b8167b721f56d6c2b20bbfeb86431f3efb56
Seggy Umboh <seg@seg.gy>
2014-09-04 23:35:07 -0700

    Fixed client config

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ internals.configStateConfig = {
     isSecure: config.isSecure
 };
 server.state('config', internals.configStateConfig);
-internals.clientConfig = {
+internals.clientConfig = JSON.stringify({
     cdnUrl: process.env.CDN_URL,
     gaPropertyId: process.env.GA_PROPERTY_ID
-}
+});
 server.ext('onPreResponse', function(request, reply) {
     if (!request.state.config) {
         var response = request.response;
