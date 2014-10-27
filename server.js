cfafdb5cffe131fb6a1bd7a64b56306c39df1357
Seggy Umboh <seg@seg.gy>
2014-10-26 21:10:32 -0700

    Added favicons

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ var balances = require('./routes/balances');
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/favicons/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'favicons')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', balances);
