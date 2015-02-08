be587d3d2737879c0f360e278f745e8a9dacae78
Seggy Umboh <seg@seg.gy>
2015-02-08 01:06:41 -0800

    Added cachify

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cachify = require('connect-cachify');
 
 var routes = require('./routes/index');
+var publicPath = path.join(__dirname, 'public');
+var assets = {
+  '/bundle.min.js': [ '/bundle.js' ]
+};
 
 var app = express();
 
@@ -11,10 +16,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/favicons/favicon.ico'));
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cachify.setup(assets, {
+  root: publicPath,
+  production: app.get('env') === 'production'
+}));
+app.use(express.static(publicPath));
 
 app.use('/', routes);
 
