0da14755215887338e8e1b8ecfa4ee6deff10c21
Seggy Umboh <seg@seg.gy>
2015-02-08 00:56:29 -0800

    Basic express routes and views

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
@@ -19,7 +17,6 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
