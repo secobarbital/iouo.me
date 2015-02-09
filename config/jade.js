56148bb03c4b3628ea798cf45eae5b5f120a6c1b
Seggy Umboh <seg@seg.gy>
2015-02-08 21:26:26 -0800

    Removed console.log

--- config/jade.js
+++ config/jade.js
@@ -9,7 +9,6 @@ var defaultOptions = {
 
 jade.__express = jade.renderFile = function(path, options, fn) {
   options = assign({}, defaultOptions, options);
-  console.log('options', options)
   return renderFile(path, options, fn);
 }
 
