e7b24849986bcacf43496c1593e739ddffe72e8d
Seggy Umboh <seg@seg.gy>
2015-02-09 23:32:19 -0800

    Flush out the other balance routes

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'iouo.me' });
 });
 
+router.get('/:ower', function(req, res, next) {
+  var ower = req.params.ower;
+  res.render('index', { title: `@${ower} on iouo.me` });
+});
+
+router.get('/:ower/:owee', function(req, res, next) {
+  var ower = req.params.ower;
+  var owee = req.params.owee;
+  res.render('index', { title: `@${owee} and @${ower} on iouo.me` });
+});
+
 module.exports = router;
