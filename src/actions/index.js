f4e3edbedf22ba11d192893ca4d257530453c5ce
Seggy Umboh <seg@seg.gy>
2015-02-11 01:56:30 -0800

    Missed files

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+var OwerActions = require('./OwerActions');
+var OweeActions = require('./OweeActions');
+
+module.exports = {
+  OwerActions: OwerActions,
+  OweeActions: OweeActions
+};
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Loading = React.createClass({
+  render: () => (
+    <section className="container">
+      <p>Loading...</p>
+    </section>
+  )
+});
+
+module.exports = Loading;
