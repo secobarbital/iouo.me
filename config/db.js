0ae864d638467db0effbbe1765218168cb80a868
Seggy Umboh <seg@seg.gy>
2015-02-18 01:13:16 -0800

    Refactored out the couchdb params building

--- config/db.js
+++ config/db.js
@@ -3,4 +3,31 @@ var url = process.env.COUCHDB_URL || process.env.CLOUDANT_URL || 'http://localho
 var dbName = process.env.COUCHDB_DATABASE || process.env.CLOUDANT_DATABASE || 'iouome'
 var db = nano(url).db.use(dbName);
 
+db.balances = function(ower, owee, stale) {
+  var params;
+  if (owee) {
+    params = {
+      descending: true,
+      reduce: false,
+      startkey: [ower, owee, {}],
+      endkey: [ower, owee],
+      include_docs: true
+    };
+  } else if (ower) {
+    params = {
+      group_level: 2,
+      startkey: [ower],
+      endkey: [ower, {}]
+    };
+  } else {
+    params = {
+      group_level: 1
+    };
+  }
+  if (stale) {
+    params.stale = 'update_after';
+  }
+  return db.view('iouome', 'balances', params);
+}
+
 module.exports = db;
--- routes/api.js
+++ routes/api.js
@@ -5,31 +5,18 @@ var db = require('../config/db');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  var params = { group_level: 1 };
-  db.view('iouome', 'balances', params).pipe(res);
+  db.balances().pipe(res);
 });
 
 router.get('/:ower', function(req, res, next) {
   var ower = req.params.ower;
-  var params = {
-    group_level: 2,
-    startkey: [ower],
-    endkey: [ower, {}]
-  };
-  db.view('iouome', 'balances', params).pipe(res);
+  db.balances(ower).pipe(res);
 });
 
 router.get('/:ower/:owee', function(req, res, next) {
   var ower = req.params.ower;
   var owee = req.params.owee;
-  var params = {
-    descending: true,
-    reduce: false,
-    startkey: [ower, owee, {}],
-    endkey: [ower, owee],
-    include_docs: true
-  };
-  db.view('iouome', 'balances', params).pipe(res);
+  db.balances(ower, owee).pipe(res);
 });
 
 module.exports = router;
