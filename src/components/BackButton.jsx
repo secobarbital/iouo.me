9c191431947fa960ab9c2c2f859a340e4de2869e
Seggy Umboh <seg@seg.gy>
2015-02-21 12:44:21 -0800

    Use fontawesome chevron so we don't load ratchicons

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,14 +1,32 @@
 var React = require('react');
 var { Link, Navigation } = require('react-router');
 
+var sx = require('../utils/styleSet');
+
 var BackButton = React.createClass({
   mixins: [Navigation],
 
   render() {
+    var { className, style, ...otherProps } = this.props;
+    className = className && className.trim() && ` ${className.trim()}`;
+    className = className || '';
+    className = `fa fa-chevron-left${className}`;
+    style = sx(styles.root, style);
     return (
-      <Link className="icon icon-left-nav pull-left" onClick={this.goBack} {...this.props} />
+      <Link className={className} style={style} onClick={this.goBack} {...otherProps} />
     );
   }
 });
 
 module.exports = BackButton;
+
+var styles = {
+  root: {
+    position: 'relative',
+    float: 'left',
+    zIndex: 20,
+    lineHeight: '44px',
+    fontSize: 17,
+    color: 'inherit'
+  }
+}
