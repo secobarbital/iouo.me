d877663397a3c777f16b70c80250a5642fc1c0c8
Seggy Umboh <seg@seg.gy>
2015-02-21 13:03:31 -0800

    Run api routes first to avoid matching ower

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(cachify);
 app.use(express.static(publicPath));
 app.use(express.static(path.join(__dirname, 'favicons')));
 
-app.use('/', routes);
 app.use('/api', api);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
