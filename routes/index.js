8f0b95d576aa33abe7b18dfeb05c804285cee099
Seggy Umboh <seg@seg.gy>
2015-02-21 12:45:57 -0800

    No more content negotiation

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,15 @@ var db = require('../config/db');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  if (!req.accepts('html')) return next();
   res.render('index', { title: 'iouo.me' });
 });
 
 router.get('/:ower', function(req, res, next) {
-  if (!req.accepts('html')) return next();
   var ower = req.params.ower;
   res.render('index', { title: `@${ower} on iouo.me` });
 });
 
 router.get('/:ower/:owee', function(req, res, next) {
-  if (!req.accepts('html')) return next();
   var ower = req.params.ower;
   var owee = req.params.owee;
   res.render('index', { title: `@${owee} and @${ower} on iouo.me` });
