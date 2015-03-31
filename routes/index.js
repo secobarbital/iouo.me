5ee02bdf047aa30913053dcb404ed1ad80ed7739
Seggy Umboh <seg@seg.gy>
2015-03-30 23:06:28 -0700

    Handle owe routes

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,19 @@ router.get('/:ower/:owee', function(req, res, next) {
   res.render('index', { title: `@${owee} and @${ower} on iouo.me` });
 });
 
+router.get('/owe', function(req, res, next) {
+  res.render('index', { title: 'Owe someone on iouo.me' });
+});
+
+router.get('/owe/:owee', function(req, res, next) {
+  var owee = req.params.owee;
+  res.render('index', { title: `Owe @${owee} on iouo.me` });
+});
+
+router.get('/owe/:ower/:owee', function(req, res, next) {
+  var ower = req.params.ower;
+  var owee = req.params.owee;
+  res.render('index', { title: `Owe @${owee} on iouo.me` });
+});
+
 module.exports = router;
