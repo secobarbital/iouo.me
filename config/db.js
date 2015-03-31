afde87956a0c40b911bcaefac6bbfac41a0acc14
Seggy Umboh <seg@seg.gy>
2015-03-30 23:01:02 -0700

    Back to nano

--- config/db.js
+++ config/db.js
@@ -1,40 +1,26 @@
-var request = require('superagent');
+var nano = require('nano');
 
 var dbUrl = process.env.COUCHDB_URL || process.env.CLOUDANT_URL || 'http://localhost:5984';
 var dbName = process.env.COUCHDB_DATABASE || process.env.CLOUDANT_DATABASE || 'iouome';
 
-var db = {
-  insert: function(body, cb) {
-    var id = body._id;
-    var req = id ? request.put : request.post;
-    req = req([dbUrl, dbName, encodeURIComponent(id)].join('/'))
-    .send(body);
-    return cb ? req.end(cb) : req;
-  },
+var db = nano(dbUrl).use(dbName);
 
-  view: function(design, view, params, cb) {
-    var req = request.get([dbUrl, dbName, '_design', design, '_view', view].join('/'))
-    .query(params);
-    return cb ? req.end(cb) : req;
-  },
-
-  pinsert: function(doc) {
-    return new Promise(function(resolve, reject) {
-      insert(doc, function(err, res) {
-        if (err) reject(err);
-        else resolve(res);
-      });
+db.pinsert = function(doc, params) {
+  return new Promise(function(resolve, reject) {
+    db.insert(doc, params, function(err, res) {
+      if (err) reject(err);
+      else resolve(res);
     });
-  },
+  });
+};
 
-  pview: function(design, view, params) {
-    return new Promise(function(resolve, reject) {
-      view(design, view, params, function(err, res) {
-        if (err) reject(err);
-        else resolve(res);
-      });
+db.pview = function(design, view, params) {
+  return new Promise(function(resolve, reject) {
+    db.view(design, view, params, function(err, res) {
+      if (err) reject(err);
+      else resolve(res);
     });
-  }
+  });
 };
 
 module.exports = db;
