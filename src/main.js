1ed2ac2e14146f25532c00300ae801bde72326bf
Seggy Umboh <seg@seg.gy>
2015-09-20 01:10:49 -0700

    Fetch data from server

--- src/main.js
+++ src/main.js
@@ -1,7 +1,44 @@
-import { run } from '@cycle/core'
-import { makeDOMDriver } from '@cycle/dom'
-import app from './app'
+/** @jsx hJSX */
 
-run(app, {
-  DOM: makeDOMDriver('main')
+import { run, Rx } from '@cycle/core'
+import { makeDOMDriver, hJSX } from '@cycle/dom'
+import { makeFetchDriver } from './fetch-driver'
+
+function main ({ DOM, Fetch }) {
+  const fetchRequest$ = Rx.Observable
+    .just({
+      url: '/owers.json'
+    })
+  const vtree$ = Fetch
+    .flatMap(res => res.json())
+    .startWith({ rows: [] })
+    .map(data => data.rows
+      .sort((a, b) => a.value - b.value)
+      .map(row => {
+        return {
+          name: row.key[0],
+          amount: row.value
+        }
+      })
+    )
+    .map(owers => {
+      let owerRows = owers
+        .map(ower => <p>{ower.name} {ower.amount}</p>)
+      return (
+        <section>
+          <h1>IOU</h1>
+          {owerRows}
+        </section>
+      )
+    })
+
+  return {
+    DOM: vtree$,
+    Fetch: fetchRequest$
+  }
+}
+
+run(main, {
+  DOM: makeDOMDriver('main'),
+  Fetch: makeFetchDriver()
 })
