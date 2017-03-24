5ddb4513f5545357b9dc32bcff5973788c6a44a7
Seggy Umboh <seg@seg.gy>
2017-03-24 02:21:25 -0700

    Use react-timeago to humanize tweets

--- components/Transaction.js
+++ components/Transaction.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import TimeAgo from 'react-timeago'
 
 export default ({ ower, owee, total, row }) => {
   const tweet = row.doc.raw
@@ -7,14 +8,13 @@ export default ({ ower, owee, total, row }) => {
   const href = `https://twitter.com/${tweet.user.id_str}/status/${tweet.id_str}`
   const avatarClass = `media-object pull-${dir}`
   const textClass = `media-body text-${dir}`
-  const timestamp = new Date(tweet.created_at)
   return (
     <a className="list-group-item media list-group-link list-group-media" href={href}>
       <img className={avatarClass} src={tweet.user.profile_image_url} />
       <div className={textClass}>
         <div>{tweet.text}</div>
         <small className="text-muted">
-          &mdash; {tweet.user.screen_name} <time datetime={timestamp.toISOString()}>at {timestamp.toString()}</time>
+          &mdash; {tweet.user.screen_name} <TimeAgo date={tweet.created_at} />
         </small>
       </div>
     </a>
