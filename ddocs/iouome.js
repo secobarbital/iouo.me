2106e47a4c8b1d6c3963c9de7ead1646897f6691
Seggy Umboh <seg@seg.gy>
2018-10-02 02:30:31 -0700

    Sort transactions by created_at

--- ddocs/iouome.js
+++ ddocs/iouome.js
@@ -14,15 +14,16 @@ module.exports = {
           ower = doc.ower.username || doc.raw.user.screen_name;
           owee = doc.owee.username || doc.raw.in_reply_to_screen_name;
           amount = doc.amount;
+          ts = doc.raw.created_at && Date.parse(doc.raw.created_at)
           if (doc.via) {
             via = doc.via.username
-            emit([ower, via], amount);
-            emit([via, owee], amount);
-            emit([via, ower], -amount);
-            emit([owee, via], -amount);
+            emit([ower, via, ts], amount);
+            emit([via, owee, ts], amount);
+            emit([via, ower, ts], -amount);
+            emit([owee, via, ts], -amount);
           } else {
-            emit([ower, owee], amount);
-            emit([owee, ower], -amount);
+            emit([ower, owee, ts], amount);
+            emit([owee, ower, ts], -amount);
           }
         }
       },
