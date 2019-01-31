4a49b753370c31d1c81ce5ab62105a0a1b5da7ef
Seggy Umboh <seg@seg.gy>
2019-01-31 10:58:02 -0800

    Fixed amount symbol

--- components/Amount.js
+++ components/Amount.js
@@ -3,6 +3,6 @@ import accounting from 'accounting-js'
 export default ({ amount }) => (
    <span>
      <span className="currency">$ </span>
-     <span className="amount">{accounting.formatMoney(Math.abs(amount), '')}</span>
+     <span className="amount">{accounting.formatMoney(Math.abs(amount), { symbol: '' })}</span>
    </span>
 )
